b5321567ec494553d50dfa81a3bf2caf7d1f2e87
zourmat <matheusmpz@hotmail.com>
2022-04-29 22:07:56 -0300

    Configurações para o servidor django - startapp base

--- pypro_mpz/settings.py
+++ pypro_mpz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7c=k(m&hxafzg$0*&ptmg1yq=@@%1duqj_4vbw*kc!kl^-tvl9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pypro_mpz.base'
 ]
 
 MIDDLEWARE = [
