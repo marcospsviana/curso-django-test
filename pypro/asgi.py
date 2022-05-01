1cab96029d6111dfbce08c75bf70f073ce396f38
marcospsviana <marcospaulo.silvaviana@gmail.com>
2022-05-01 18:54:05 -0300

    project full uploaded

--- pypro/asgi.py
+++ pypro/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for pypro_mpz project.
+ASGI config for pypro project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pypro_mpz.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pypro.settings')
 
 application = get_asgi_application()
